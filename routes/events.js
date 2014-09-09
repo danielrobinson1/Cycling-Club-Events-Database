eccc60285f8a340dc18d9a82a6ffdb161e9f2ac6
danielrobinson1 <dannyhelicopter@hotmail.com>
2014-09-09 17:42:09 +0100

    Removed comments.

--- routes/events.js
+++ routes/events.js
@@ -1,14 +1,3 @@
-/* 
-
-These route need work.
-
-They were copied from the wheelers_events_app project which
-used a local database with mong and monk.
-
-The routes need modifying to work with mongoose and our Event model.
-
-*/
-
 var express = require('express');
 var router  = express.Router();
 
@@ -21,22 +10,11 @@ router
 
 	/*
 
-	POST Add an event to the database
+	POST Add an event to the database.
 
-	New request works with mongoose.
-
-	TODO Remove json message and setup to get
-	values from the form as in the old POST request
-	below.
+	TODO Redirect to eventslist page.
 
 	*/
-
-	// Changes
-
-	// Changed URL to from / to /addevent to match the form "action".
-	// May need to change the form action to "events/addevent".
-
-	// TODO Redirect to eventslist page.
 
 	.post('/addevent', function(req, res) {
 		
@@ -68,61 +46,12 @@ router
 		
 	})
 
-	
-		/*
-
-		Old request worked with mongo and monk.
-
-		TODO Delete once functionality has been transferred
-		to new request.
-
-		*/
-
-		/*
-
-		.post('/addevent', function(req, res) {
-
-			// Set our internal DB variable
-			var db = req.db;
-
-			// Get our form values, using 'name' attributes
-			var eventDate = req.body.date;
-			var eventName = req.body.name;
-			var eventType = req.body.type;
-
-			// Set our collection
-			var collection = db.get('eventcollection');
-
-			// Submit to the DB
-			collection.insert({
-				"date" : eventDate,
-				"name" : eventName,
-				"type" : eventType
-			}, function(err, doc) {
-				if (err) {
-					// If it failed, return error
-					res.send("There was a problem adding the event to the database.")
-				}
-				else {
-					// If it worked, set the header so the adress bar doesn't still say /addevent.
-					res.location("eventlist");
-					// And forward to success page
-					res.redirect("eventlist");
-				}
-			});
-		});
-
-		*/
-
 
 	/*
 
-	GET request to get all events in the db
+	GET Retrieve all events in the database 
 
-	New GET request.
-
-	TODO Remove jso message and return events in the table
-	in the eventlist view
+	and render the eventlist view.
 
 	*/
 
@@ -136,28 +65,5 @@ router
         	});
 		});
 	});
-
-		/*
-
-		Old GET request. Works woith mongo and monk.
-
-		TODO Delete once functionality has been 
-		transferred to new request.
-
-		*/
-
-		/*
-
-		.get('/eventlist', function(req, res) {
-			var db = req.db;
-			var collection = db.get('eventcollection');
-			collection.find({},{},function(e,docs){
-				res.render('eventlist', {
-					"eventlist" : docs, title: 'Manchester Wheelers | Event List'
-				});
-			});
-		});
-
-		*/
 
 module.exports = router;
