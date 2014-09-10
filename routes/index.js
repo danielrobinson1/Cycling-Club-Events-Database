3541eb44af3f07b8f2e73d3495d33026c92f2a23
danielrobinson1 <dannyhelicopter@hotmail.com>
2014-09-10 11:15:52 +0100

    Added test delete route to index.js
    Also added GET request to index.js to return all events in json format for
    purposes of testing in Postman Client.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,40 @@
+/* Testing new routes */
+
 var express = require('express');
 var router  = express.Router();
 
+// Import Event model
+
+var Event   = require('../models/event');
+
 router
+
+// GET Returns all events in json for purposes of testing in Postman CLient.
+// Working
+
+.get('/test/list', function(req, res) {
+		Event.find(function(err, events) {
+			if (err)
+				res.send(err);
+
+			res.json(events);
+		});
+	})
+
+// DELETE Remove event with the specified id.
+// Working
+
+	.delete('/test/delete/:event_id', function(req, res) {
+		Event.remove({
+			_id: req.params.event_id
+		}, function(err, event) {
+			if (err)
+				res.send(err);
+
+			res.json({ message: 'Successfully deleted' });
+		});
+	});
+
 
 
 module.exports = router;
